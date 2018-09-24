fef557e75ff24cb677c09495714bd1b2d302e2d4
jabier espinal <jabierespinal@gmail.com>
2018-09-24 23:31:07 +0000

    added a note

--- python.py
+++ python.py
@@ -6,3 +6,12 @@ a = "I am back to learning to code again" # string
 print(a)
 a = 30   # changes value of a to int 30
 print(a)
+
+a = type(.5) # displays the type of number  float
+print(a)
+a= type(5)   # displays the type of number  int
+print(a)   # prints variable
+
+
+
+
