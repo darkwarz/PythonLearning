57829ab66bb38b353a9435d701c6ed8320b3d436
jabier espinal <jabierespinal@gmail.com>
2018-10-05 02:09:32 +0000

    math.py added with some simple variables declared with simple math

--- python.py
+++ python.py
@@ -12,6 +12,9 @@ print(a)
 a= type(5)   # displays the type of number  int
 print(a)   # prints variable
 
+#a = 7.5 % 5 = 2.5  modulo remainder is 2.5 
 
 
-
+# health = 50
+#import random
+#potion_health = random.randint(25,50)       25 is minimum range and 50 is max. and it will produce a random number
