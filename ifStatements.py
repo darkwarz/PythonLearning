dd1e294f35d38f0afc542a485cbaba7b113d4995
jabier espinal <jabierespinal@gmail.com>
2019-01-28 18:02:02 +0000

    updated ifStatements.py

--- ifStatements.py
+++ ifStatements.py
@@ -3,5 +3,9 @@ num2 = 200
 
 if num1 > num2:
     print("num1 is bigger than num2")
-else:
+
+elif num1 == num2:
+    print("num1 is equal to num2")
+    
+elif num1 < num2:
     print("num2 is bigger than num1")
